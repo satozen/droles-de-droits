5143be4ac3c8f0572f31327108f66fe7c6c05fab
Dominic St-Laurent <dstl.st.laurent@gmail.com>
2025-10-29 22:09:32 -0400

    Configure GitHub Pages deployment with static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Si votre repo s'appelle 'droles-de-droits', décommentez la ligne suivante:
+  // basePath: '/droles-de-droits',
 }
 
 module.exports = nextConfig
